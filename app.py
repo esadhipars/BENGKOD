ae6819e3ea51a0ba7c748cb74c2fae08ad157552
esadhipars <masdhipa@gmail.com>
2025-06-14 14:49:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Import libraries yang dibutuhkan
 import streamlit as st
 import pickle
 import numpy as np
@@ -19,6 +18,17 @@ with open("model_columns.pkl", "rb") as f:
     model_columns = pickle.load(f)
 
 
+# Mapping label prediksi
+label_map = {
+    0: "Insufficient Weight",
+    1: "Normal Weight",
+    2: "Overweight Level I",
+    3: "Overweight Level II",
+    4: "Obesity Type I",
+    5: "Obesity Type II",
+    6: "Obesity Type III",
+}
+
 # Input data
 age = st.number_input("Usia", min_value=0, max_value=100, value=25)
 height = st.number_input("Tinggi Badan (m)", min_value=0.0, max_value=2.5, value=1.7)
@@ -36,41 +46,43 @@ tue = st.number_input("Jumlah Jam Menggunakan Perangkat Elektronik per Hari", mi
 calc = st.selectbox("Sering Mengonsumsi Alkohol?", ["Sometimes", "Frequently", "Always", "No"])
 mtrans = st.selectbox("Transportasi yang Digunakan", ["Automobile", "Motorbike", "Public Transportation", "Walking"])
 
-# Encode sesuai training
-input_data = pd.DataFrame([[
-    age, height, weight, 
-    1 if family_history == "yes" else 0,
-    1 if favc == "yes" else 0,
-    fcvc, ncp, 
-    0 if caec == "No" else (1 if caec == "Sometimes" else (2 if caec == "Frequently" else 3)),
-    1 if smoke == "yes" else 0,
-    ch2o,
-    1 if scc == "yes" else 0,
-    faf,
-    tue,
-    0 if calc == "No" else (1 if calc == "Sometimes" else (2 if calc == "Frequently" else 3)),
-    0 if mtrans == "Walking" else (1 if mtrans == "Public Transportation" else (2 if mtrans == "Motorbike" else 3))
-]], columns=[
-    'Age', 'Height', 'Weight',
-    'family_history_with_overweight',
-    'FAVC', 'FCVC', 'NCP',
-    'CAEC', 'SMOKE', 'CH2O',
-    'SCC', 'FAF', 'TUE',
-    'CALC', 'MTRANS'
-])
-
-# Mengatur sesuai urutan kolom yang diberlakukan saat training
-input_data = input_data.reindex(columns=model_columns, fill_value=0)
-
-# Mengkonversi tipe data ke float
-input_data = input_data.astype(float)
-
-
 # Tombol prediksi
 if st.button("Prediksi"):
+
+    # Mengumpulkan data yang diberika­n
+    input_data = pd.DataFrame([[
+        age, height, weight, 
+        1 if family_history == "yes" else 0,
+        1 if favc == "yes" else 0,
+        fcvc, ncp, 
+        0 if caec == "No" else (1 if caec == "Sometimes" else (2 if caec == "Frequently" else 3)),
+        1 if smoke == "yes" else 0,
+        ch2o,
+        1 if scc == "yes" else 0,
+        faf,
+        tue,
+        0 if calc == "No" else (1 if calc == "Sometimes" else (2 if calc == "Frequently" else 3)),
+        0 if mtrans == "Walking" else (1 if mtrans == "Public Transportation" else (2 if mtrans == "Motorbike" else 3))
+    ]], columns=[
+        'Age', 'Height', 'Weight',
+        'family_history_with_overweight',
+        'FAVC', 'FCVC', 'NCP',
+        'CAEC', 'SMOKE', 'CH2O',
+        'SCC', 'FAF', 'TUE',
+        'CALC', 'MTRANS'
+    ]) 
+
+
+    # Mengatur sesuai urutan kolom yang diberlakukan saat training
+    input_data = input_data.reindex(columns=model_columns, fill_value=0)
+
+
+    # Mengkonversi tipe data ke float
+    input_data = input_data.astype(float)
+
 
     # Prediksi
     prediction = model.predict(input_data)[0]
 
-    st.success(f"Prediksi Tingkat Obesitas : {prediction}")
+    st.success(f"Prediksi Tingkat Obesitas : {label_map[prediction]}")
 
